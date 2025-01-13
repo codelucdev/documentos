fcd73a2c5b11be43419cfd4c2bd66868552eaa45
codelucdev <146024482+codelucdev@users.noreply.github.com>
2025-01-13 11:21:21 -0300

    Update script.js

--- index-Crural/script/script.js
+++ index-Crural/script/script.js
@@ -59,7 +59,7 @@ document.getElementById('gerarPDF').addEventListener('click', function() {
             <h1 style="color: black; text-align: center; text-decoration:underline; font-size:25px; margin: 120px; ">DECLARAÇÃO DE NÃO IMPEDIMENTO RURAL</h1>
             <div class="pdf-content">
                 <p style="text-align:justify; margin:50px;">A PREFEITURA DE SÃO JOÃO DA PONTE, sediada na PRAÇA OLÍMPIO CAMPOS, Nº 128, CENTRO, nesta cidade de SÃO JOÃO DA PONTE, ora representada pelo Sr. ${responsavel}, Declara que NÃO HÁ IMPEDIMENTOS PARA LIGAÇÃO de ENERGIA ELÉTRICA em uma ÁREA RURAL com área de aproximadamente ${area} ${unidade}, situado em um lugar denominado ${localizacao}, situada na região de ${regiao}, neste município, de responsabilidade de ${proprietario}, inscrito no ${documento.toUpperCase()}: ${cpf_cnpj.toUpperCase()}, SN, neste município de São João da Ponte - MG.</p>
-                <p style="text-align:justify; margin-left:50px;">Número de Controle: ${numero_controle}-2</p>
+                <p style="text-align:justify; margin-left:50px;">Número de Controle: ${numero_controle}</p>
                 <p style="margin-left:50px">Área do Terreno: ${area}${unidade}</p>
                 <p style="text-align:justify; margin-left:50px;">São João da Ponte – MG, ${data}.</p>
             </div>
